e33877ad3b8a7fca67ca5d20411b3dc5e47b13b7
Ray Flores <ray.m.flores@gmail.com>
2022-03-15 03:04:09 -0400

    Create basic structure of question card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,6 +1,39 @@
 import React from "react";
 
-const QuestionCard = () => <div>Question Card</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNumber: number;
+  totalQuestions: number;
+}
+
+// functional component
+const QuestionCard: React.FC<Props> = ({ 
+  question, 
+  answers, 
+  callback, 
+  userAnswer, 
+  questionNumber, 
+  totalQuestions 
+}) =>  (
+<div>
+  <p className="questionNumber">
+    Question: {questionNumber} / {totalQuestions}
+  </p>
+  <p dangerouslySetInnerHTML={{ __html: question }}></p>
+  <div>
+    {answers.map(answer => (
+      <div>
+        <button disabled={userAnswer} onClick={callback}>
+          <span dangerouslySetInnerHTML={{ __html: answer }} />
+        </button>
+      </div>
+    ))}
+  </div>
+</div>
+);
 
 export default QuestionCard;
 
