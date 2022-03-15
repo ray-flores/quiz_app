3249fac3a0385df7b42385c137f48842ca4dc275
Ray Flores <ray.m.flores@gmail.com>
2022-03-15 18:33:21 -0400

    Set types for states for answers and questions arrays

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,23 @@ import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
 // Types
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
 
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGaveOver] = useState(true);
 
