1d4d4f66884263af846848cf0c57090e8e0fea86
Ray Flores <ray.m.flores@gmail.com>
2022-03-15 17:02:24 -0400

    Implement better shuffle questions function

--- src/API.ts
+++ src/API.ts
@@ -1,10 +1,10 @@
-
+import { shuffleArray } from "./utils";
 
 export type Question = {
   category: string;
   correct_answer: string;
   difficulty: string;
-  incorrect_answer: string[];
+  incorrect_answers: string[];
   question: string;
   type: string;
 }
@@ -28,7 +28,9 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
   return data.results.map((question: Question) => (
     {
       ...question,
-      answer: 
+      answers: shuffleArray([
+        ...question.incorrect_answers, 
+        question.correct_answer])
     }
   ))
 } 
--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,23 @@
 // function to randomize answers to questions 
 
 export const shuffleArray = (array: any[]) => {
-  [...array].sort(() => Math.random() - 0.5);
+
+  //Fisher-Yates Shuffle
+  let currentIndex = array.length, randomIndex;
+
+  // while there are elements to shuffle..
+  while (currentIndex) {
+
+    // choose a remaining element..
+    randomIndex = Math.floor(Math.random() * currentIndex--);
+
+    // and swap it with current element.
+    [array[currentIndex], array[randomIndex]] = 
+    [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+
+  // // crude shuffle function:
+  // [...array].sort(() => Math.random() - 0.5);
 }
