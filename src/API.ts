fc019fbf710480e72c91b5950760396c15b85345
Ray Flores <ray.m.flores@gmail.com>
2022-03-15 03:18:21 -0400

    Add api logic

--- src/API.ts
+++ src/API.ts
@@ -1,1 +1,12 @@
+
+
+export enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard'
+}
+
 // logic for fetching data from API
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+  
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+
+
+import React, { useState } from 'react';
 // Components
 import QuestionCard from './components/QuestionCard';
 
 
-
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGaveOver] = useState(true);
+
 
   // make API call
   const startQuiz = async () => {
@@ -32,7 +42,15 @@ const App = () => {
       </button>
       <p className='score'>Score:</p>
       <p>Loading Questions... </p>
-      <QuestionCard />
+      <QuestionCard 
+        // add one because first item in array has index 0
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
