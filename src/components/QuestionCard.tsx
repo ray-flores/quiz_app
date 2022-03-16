fc5ff9e609fb985c12bdbaa61850c0851091ce80
Ray Flores <ray.m.flores@gmail.com>
2022-03-16 01:41:02 -0400

    Remove type 'any' from props object properties

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+// types
+import { AnswerObject } from "../App";
+// styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
+
 type Props = {
   question: string;
   answers: string[];
-  callback: any;
-  userAnswer: any;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNumber: number;
   totalQuestions: number;
 }
@@ -26,7 +31,7 @@ const QuestionCard: React.FC<Props> = ({
   <div>
     {answers.map(answer => (
       <div key={answer}>
-        <button disabled={userAnswer} value={answer} onClick={callback}>
+        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
           <span dangerouslySetInnerHTML={{ __html: answer }} />
         </button>
       </div>
