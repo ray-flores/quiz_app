d92375024af34345c9ef2837fd0bad7e95417a25
Ray Flores <ray.m.flores@gmail.com>
2022-03-16 01:44:10 -0400

    Show score in app and add styles files for app and question card

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ const App = () => {
         </button>
       ) : null}
 
-      {!gameOver ? <p className='score'>Score:</p> : null}
+      {!gameOver ? <p className='score'>Score: {score}</p> : null}
 
       {loading && <p>Loading Questions... </p>}
 
